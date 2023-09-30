521f66fe0f1c48f3fdc907a05dac8a1e2308e08a
fixploit14 <145822949+fixploit14@users.noreply.github.com>
2023-10-01 00:08:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # install
+# https://github.com/fixploit14/apache2-login.git
 
+sudo bash create_user.sh
 sudo cp 000-default.conf /etc/apache2/sites-enabled/
 sudo systemctl restart apache2
 sudo cp apache2.conf /etc/apache2/
